7c9eed6aaf0e9517d82d23b6b6f5a0df7c3a6f39
Timibadass <oblacishere@gmail.com>
2020-05-09 08:51:40 +0100

    set up vue-i18n

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ $t('hello') }}</p>
+    <p>{{ $t('greetings') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n"
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!",
+    "greetings": "Welcome to component"
+  }
+}
+</i18n>
